79e53bb2b236d9c454ab54e0552a5eaa929ba4c7
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-12-08 06:11:28 +0000

    fixed candidate class

--- src/packagefiles/Candidate.java
+++ src/packagefiles/Candidate.java
@@ -36,8 +36,93 @@ public class Candidate {
     int year3 = 0;
     int year4 = 0;
 
+    public int getAge1820() {
+        return age1820;
+    }
+
+    public int getAge2124() {
+        return age2124;
+    }
+
+    public int getAge2529() {
+        return age2529;
+    }
+
+    public int getAge3039() {
+        return age3039;
+    }
+
+    public int getAge40() {
+        return age40;
+    }
+
+    public int getGenderMale() {
+        return genderMale;
+    }
+
+    public int getGenderFemale() {
+        return genderFemale;
+    }
+
+    public int getGenderOther() {
+        return genderOther;
+    }
+
+    public int getOriginUK() {
+        return originUK;
+    }
+
+    public int getOriginEEA() {
+        return originEEA;
+    }
+
+    public int getOriginInternational() {
+        return originInternational;
+    }
+
+    public int getCourseCS() {
+        return courseCS;
+    }
+
+    public int getCourseCG() {
+        return courseCG;
+    }
+
+    public int getCourseWD() {
+        return courseWD;
+    }
+
+    public int getCourseIT() {
+        return courseIT;
+    }
+
+    public int getCourseITMB() {
+        return courseITMB;
+    }
+
+    public int getYear0() {
+        return year0;
+    }
+
+    public int getYear1() {
+        return year1;
+    }
+
+    public int getYear2() {
+        return year2;
+    }
+
+    public int getYear3() {
+        return year3;
+    }
+
+    public int getYear4() {
+        return year4;
+    }
+
     Candidate(String name, String ageGroup, String genderType, String origin, String courseName, String year) {
         this.name = name;
+
         switch (ageGroup) {
             case "age1820" : age1820++;
             case "age2124" : age2124++;
@@ -69,6 +154,39 @@ public class Candidate {
             case "year3" : year3++;
             case "year4" : year4++;
         }
-
+    }
+    // testing to see if this works
+    protected void updateStats(String ageGroup, String genderType, String origin, String courseName, String year) {
+        switch (ageGroup) {
+            case "age1820" : this.age1820++;
+            case "age2124" : this.age2124++;
+            case "age2529" : this.age2529++;
+            case "age3039" : this.age3039++;
+            case "age40" :   this.age40++;
+        }
+        switch (genderType) {
+            case "genderMale" : this.genderMale++;
+            case "genderFemale" : this.genderFemale++;
+            case "genderOther" : this.genderOther++;
+        }
+        switch (origin) {
+            case "originUK" : originUK++;
+            case "originEEA" : originEEA++;
+            case "originInternational" : originInternational++;
+        }
+        switch (courseName) {
+            case "courseCS" : courseCS++;
+            case "courseCG" : courseCG++;
+            case "courseWD" : courseWD++;
+            case "courseIT" : courseIT++;
+            case "courseITMB" : courseITMB++;
+        }
+        switch (year) {
+            case "year0" : year0++;
+            case "year1" : year1++;
+            case "year2" : year2++;
+            case "year3" : year3++;
+            case "year4" : year4++;
+        }
     }
 }
--- src/packagefiles/Main.java
+++ src/packagefiles/Main.java
@@ -5,40 +5,61 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import packagefiles.Candidate.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main extends Application {
 
-    public static String[] nameList = new String[0];
+    public static Candidate nameList = new String[0];
     public static int[] votesList = new int[0];
+    public static int numberOfCandidates = 0;
+
+    /*
     public static Map<String,Integer> ageMap = new HashMap<>();
     public static Map<String,Integer> genderMap = new HashMap<>();
     public static Map<String,Integer> originMap = new HashMap<>();
     public static Map<String,Integer> courseMap = new HashMap<>();
     public static Map<String,Integer> yearMap = new HashMap<>();
 
-    public static int numberOfCandidates = 0;
 
 
+    public static void incrementValue(Map<String,Integer> map, String name) {
+        Integer value = map.get(name);
+        if (value == null) {
+            map.put(name, 1);
+        }
+        else {
+            map.put(name, value + 1);
+        }
+    }
+
+
+     */
     public static void addData(String name, String age, String gender, String origin, String course, String year) {
-        add2Array(nameList,name);
-        // space for voteList
+        int indexOfName = findCandidate(name);
+        if (indexOfName >= 0) {
+            votesList[indexOfName]++;
+            itemList[indexOfName].updateStats(age, gender, origin, course, year);
+        }
+        else {
+            add2ArrayString(name);
+            add2ArrayInt();
+        }
+
     }
 
     /**
      * Adds one item to the String[]
-     * @param src Source/original array[]
      * @param newValue the item to be added
      */
-    public static void add2ArrayString(String[] src, String newValue) {
-        if (numberOfCandidates >= src.length) {
-            String[] temp = new String[src.length * 2 + 1]; // +1 to avoid multiplication with size 0
-            System.arraycopy(src, 0,temp, 0, src.length);
-            src = temp;
+    public static void add2ArrayString(String newValue) {
+        if (numberOfCandidates >= nameList.length) {
+            String[] temp = new String[nameList.length * 2 + 1]; // +1 to avoid multiplication with size 0
+            System.arraycopy(nameList, 0,temp, 0, nameList.length);
+            nameList = temp;
         }
-        src[numberOfCandidates++] = newValue;
+        nameList[numberOfCandidates++] = newValue;
     }
     /**
      * Adds one item to the int[]. This one is different the number of candidates
@@ -46,25 +67,25 @@ public class Main extends Application {
      * @param src Source/original array[]
      * @param newValue the item to be added
      */
-    public static void add2ArrayInt(int[] src, int newValue) {
-        if (numberOfCandidates >= src.length) {
-            int[] temp = new int[src.length * 2 + 1]; // +1 to avoid multiplication with size 0
-            System.arraycopy(src, 0,temp, 0, src.length);
-            src = temp;
+    public static void add2ArrayInt() {
+        if (numberOfCandidates >= votesList.length) {
+            int[] temp = new int[votesList.length * 2 + 1]; // +1 to avoid multiplication with size 0
+            System.arraycopy(votesList, 0,temp, 0, votesList.length);
+            votesList = temp;
         }
-        src[numberOfCandidates] = newValue;
+        votesList[numberOfCandidates] = 1; // increases the vote tally by one for selected index
     }
 
     /**
      *
      * @param name name of the candidate taken from the inputField
-     * @return boolean
+     * @return int
      */
-    public  static boolean  findCandidate(String name) {
-        for (String s : nameList) {
-            if (s.equals(name)) return true;
-        }
-        return false;
+    public  static int findCandidate(String name) {
+       for (int i = 0; i < nameList.length; i++) {
+           if (nameList[i].equals(name)) return i;
+       }
+       return -1;
     }
 
     /**
